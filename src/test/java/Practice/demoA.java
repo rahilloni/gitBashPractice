a631cc4480da1cbd6e2a50128e1847ce9bf024fa
rahilloni <mohammedrahilloni@gmail.com>
2022-03-01 17:53:46 +0530

    modified few code

--- src/test/java/Practice/demoA.java
+++ src/test/java/Practice/demoA.java
@@ -3,7 +3,11 @@ package Practice;
 public class demoA {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		
+		
+		System.out.println("Hello all");
 
 	}
 
